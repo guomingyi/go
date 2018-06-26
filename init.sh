1750fbd9f299ffbcfafb5b72c326ec4f663e65b0
gmy <296778322@qq.com>
2018-06-26 14:31:03 +0800

    <rebuild libui_linux_amd64.a >

--- init.sh
+++ init.sh
@@ -4,11 +4,10 @@ mypath=$PWD
 mkdir -p $mypath/src/github.com/andlabs
 cd $mypath/src/github.com/andlabs
 
-git clone git@github.com:andlabs/ui.git
-
-git clone git@github.com:andlabs/libui.git
+git clone https://github.com/andlabs/ui
+git clone https://github.com/andlabs/libui.git
 cd libui && git checkout alpha3.5
-mkdir build && cd build
+rm -rf build && mkdir build && cd build
 
 cmake -DBUILD_SHARED_LIBS=OFF .. && make
 cp out/libui.a $mypath/src/github.com/andlabs/ui/libui_linux_amd64.a
